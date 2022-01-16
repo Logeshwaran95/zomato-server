550467aa1700a899098986e76d84b92ecbb599a5
Logeshwaran <logeshsiva395@gmail.com>
2022-01-16 17:47:56 +0530

    final

--- src/API/Auth/index.js
+++ src/API/Auth/index.js
@@ -75,7 +75,7 @@ Router.get(
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
     return res.redirect(
-      `http://localhost:3000/google/${req.session.passport.user.token}`
+      `https://dreamy-spence-a6fed7.netlify.app/google/${req.session.passport.user.token}`
     );
   }
 );
